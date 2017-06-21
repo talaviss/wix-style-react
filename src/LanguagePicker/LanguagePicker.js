f6bef5067b0448ed11e5b81f9d7cdf5c01569467
Lior Belinsky <liorbe@wix.com>
2017-06-21 23:46:49 +0300

    update languagePicker

--- src/LanguagePicker/LanguagePicker.js
+++ src/LanguagePicker/LanguagePicker.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import WixComponent from '../WixComponent';
-import PopoverMenu from '../PopoverMenu/PopoverMenu';
-import PopoverMenuItem from '../PopoverMenuItem/PopoverMenuItem';
-import {PenOutline} from '../Icons/dist';
-
-const defaultLanguages = ['en', 'es'];
+import {Languages} from '../Icons/dist';
+import ButtonWithOptions from '../ButtonWithOptions/ButtonWithOptions';
+import Button from '../Button/Button';
 
 export default class LanguagePicker extends WixComponent {
   onSelect(locale) {
@@ -14,29 +12,39 @@ export default class LanguagePicker extends WixComponent {
   }
 
   render() {
-    const {dataHook, availableLanguages} = this.props;
+    const {dataHook, theme, children} = this.props;
     return (
-      <PopoverMenu dataHook={dataHook} placement="bottom">
-        {
-          availableLanguages.map(lang => (
-          <PopoverMenuItem
-            icon={<PenOutline />}
-            onClick={() => this.onSelect()}
-            text="Edit"
-          />
-        ))
-        }
-      </PopoverMenu>
+      <ButtonWithOptions
+        dataHook={dataHook}
+        onSelect={option => this.onSelect(option.value)}
+        >
+        <Button type="button" height="medium" theme={theme}>
+          <Languages size="12px"/>
+        </Button>
+        {children}
+      </ButtonWithOptions>
     );
   }
 }
 
 LanguagePicker.defaultProps = {
-  availableLanguages: defaultLanguages
-}
+  theme: 'icon-greybackground'
+};
 
 LanguagePicker.propTypes = {
   dataHook: PropTypes.string,
   availableLanguages: PropTypes.arrayOf(PropTypes.string),
-  i18n: PropTypes.func.isRequired
+  i18n: PropTypes.func.isRequired,
+  baseUrl: PropTypes.string.isRequired,
+  theme: PropTypes.string,
+  children: PropTypes.any
+};
+
+LanguagePicker.Option = ({languageKey, children}) => (
+  <ButtonWithOptions.Option id={languageKey}>{children}</ButtonWithOptions.Option>
+);
+
+LanguagePicker.Option.propTypes = {
+  languageKey: PropTypes.string.isRequired,
+  children: PropTypes.string
 };
--- src/LanguagePicker/README.md
+++ src/LanguagePicker/README.md
@@ -5,7 +5,18 @@
 | propName | propType | defaultValue | isRequired | description |
 |----------|----------|--------------|------------|-------------|
 | dataHook | string | - | - | Specifies a data-hook for tests |
-| i18n | func | - | + |  |
-| baseUrl | string | - | + |  |
-| availableLanguages | array | - | - |  |
-| onSelect | func | - | - |  |
+| children | nodes | - | + | Specify the languages list to render |
+| i18n | func | - | + | The function you pass to the I18nextProvider in your production code |
+| baseUrl | string | - | + | baseUrl param for the i18n translation |
+| onSelect | func | - | - | Callback to call on language selection |
+| theme | string | icon-greybackground | - | Theme of the icon's background |
+
+
+# LanguagePicker.Option
+
+## Properties
+
+| propName | propType | defaultValue | isRequired | description |
+|----------|----------|--------------|------------|-------------|
+| languageKey | string | - | + | language key such as 'en' that suits the translation file name |
+| children | string | - | + | The language name to display in the dropdown's options |
